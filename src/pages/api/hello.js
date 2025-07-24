2e0760c79f08bead6d5b2f832f79ea006de7f52c
ahmadfaridfiza <ahmadfaridfiza@gmail.com>
2025-07-24 19:41:55 +0800

    ap

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,22 +1,30 @@
-export const config = {
-  runtime: 'nodejs'
-};
 
-import { Account, deriveAccount } from "@aptos-labs/ts-sdk";
-import bip39 from "bip39";
+import { Account, Aptos, AptosConfig, Network, SigningSchemeInput } from "@aptos-labs/ts-sdk";
 
 export default async function handler(req, res) {
-  const mnemonic = bip39.generateMnemonic();
+  try {
+    // Setup Aptos Client (Devnet)
+    const config = new AptosConfig({ network: Network.DEVNET });
+    const aptos = new Aptos(config);
 
-  // Derive Account using Aptos official SDK derivation path m/44'/637'/0'/0'/0'
-  const account = await deriveAccount({
-    mnemonic,
-    derivationPath: "m/44'/637'/0'/0'/0'"
-  });
+    // Generate Account (Ed25519 Legacy, no mnemonic)
+    const account = Account.generate({
+      scheme: SigningSchemeInput.Ed25519,
+      legacy: true
+    });
 
-  res.status(200).json({
-    mnemonic,
-    privateKey: account.privateKeyHex,
-    address: account.accountAddress.toString()
-  });
+    // Fund Account via Faucet
+    await aptos.fundAccount({
+      accountAddress: account.accountAddress,
+      amount: 100_000_000 // 100 APT
+    });
+
+    // Return JSON Response
+    res.status(200).json({
+      address: account.accountAddress.toString(),
+      privateKey: account.privateKeyHex
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
