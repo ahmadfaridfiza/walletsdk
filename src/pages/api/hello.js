3ff7685a7feecf0d9b0f2bb8de2636814a5e36ab
ahmadfaridfiza <ahmadfaridfiza@gmail.com>
2025-07-24 19:02:54 +0800

    ap

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { AptosAccount, Ed25519PrivateKey } from "aptos";
+import bip39 from "bip39";
+import { derivePath } from "ed25519-hd-key";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  const mnemonic = bip39.generateMnemonic();
+  const seed = bip39.mnemonicToSeedSync(mnemonic);
+
+  const path = "m/44'/637'/0'/0'/0'";
+  const { key } = derivePath(path, seed.toString('hex'));
+
+  const privateKey = new Ed25519PrivateKey(Buffer.from(key, 'hex'));
+  const account = AptosAccount.fromPrivateKey(privateKey);
+
+  res.status(200).json({
+    mnemonic,
+    privateKey: account.signingKey.secretKey.toString('hex').slice(0, 64),
+    address: account.address().toString()
+  });
 }
