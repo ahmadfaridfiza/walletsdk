2dbb436625f4fdde869419ee03933b0b72732c51
ahmadfaridfiza <ahmadfaridfiza@gmail.com>
2025-07-24 20:12:18 +0800

    ap

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,18 +1,31 @@
-import { Account, Aptos, AptosConfig, Network, SigningSchemeInput } from "@aptos-labs/ts-sdk";
+import { Account, derivePath } from "@aptos-labs/ts-sdk";
+const bip39 = require('bip39');
 
 export default async function handler(req, res) {
   try {
-    const privateKey = new Ed25519PrivateKey("myEd25519privatekeystring");
+    const mnemonic = bip39.generateMnemonic(128);
 
-const aptos = new Aptos();
-const account = await aptos.deriveAccountFromPrivateKey({ privateKey });
+    // Derive seed from mnemonic
+    const seed = await bip39.mnemonicToSeed(mnemonic);
+
+    // Aptos derivation path
+    const derivationPath = "m/44'/637'/0'/0'/0'";
+    const { key } = derivePath(derivationPath, seed.toString('hex'));
+
+    // Create Account
+    const account = Account.fromDerivePath(mnemonic, derivationPath);
+
+    // Extract PrivateKey (first 64 chars of secretKey)
+    const privateKeyHex = Buffer.from(account.signingKey.secretKey).toString('hex').slice(0, 64);
 
     // Return JSON Response
     res.status(200).json({
-      address: account.toString(),
-      privateKey: privateKey
+      mnemonic,
+      address: account.accountAddress.toString(),
+      privateKey: privateKeyHex
     });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: error.message });
   }
 }
