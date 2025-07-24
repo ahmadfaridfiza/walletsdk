d30010cd6cf2cb68c182c87ed8551a5803632fd6
ahmadfaridfiza <ahmadfaridfiza@gmail.com>
2025-07-24 23:22:26 +0800

    ap

--- src/pages/api/cardano.js
+++ src/pages/api/cardano.js
@@ -1,5 +1,5 @@
 import * as bip39 from 'bip39';
-import CardanoWasm from '@emurgo/cardano-serialization-lib-nodejs';
+import * as CardanoWasm from '@emurgo/cardano-serialization-lib-nodejs';
 
 export default async function handler(req, res) {
   try {
@@ -16,22 +16,29 @@ export default async function handler(req, res) {
     );
 
     // Derive Account Key (m/1852'/1815'/0')
-    const purposeKey = rootKey.derive(1852 | 0x80000000);
-    const coinTypeKey = purposeKey.derive(1815 | 0x80000000);
-    const accountKey = coinTypeKey.derive(0 | 0x80000000);
+    const accountKey = rootKey
+      .derive(1852 | 0x80000000)
+      .derive(1815 | 0x80000000)
+      .derive(0 | 0x80000000);
 
     // Derive External Address Key (m/1852'/1815'/0'/0/0)
     const externalKey = accountKey.derive(0).derive(0);
 
-    // Get Private Key in hex
+    // Get Private Key Hex
     const privateKeyHex = Buffer.from(externalKey.to_raw_key().as_bytes()).toString('hex');
 
-    // Get Public Key & Address
+    // Get Public Key & Stake Key Hash
     const publicKey = externalKey.to_public();
+    const keyHash = publicKey.hash();
+
+    // Create StakeCredential from KeyHash
+    const stakeCredential = CardanoWasm.StakeCredential.from_keyhash(keyHash);
+
+    // Create Base Address
     const baseAddr = CardanoWasm.BaseAddress.new(
-      0, // Mainnet = 0, Testnet = 1
-      CardanoWasm.StakeCredential.from_keyhash(publicKey.to_raw_key().hash()),
-      CardanoWasm.StakeCredential.from_keyhash(publicKey.to_raw_key().hash())
+      0, // Mainnet = 0
+      stakeCredential,
+      stakeCredential
     );
 
     const address = baseAddr.to_address().to_bech32();
