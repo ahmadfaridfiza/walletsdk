ca1e236ffc1dd1ca1d5349a8d635a6cdd6643106
ahmadfaridfiza <ahmadfaridfiza@gmail.com>
2025-07-25 00:28:24 +0800

    ap

--- src/pages/api/doge.js
+++ src/pages/api/doge.js
@@ -1,27 +1,26 @@
+import * as bip39 from 'bip39';
+import * as bitcoin from 'bitcoinjs-lib';
+
+const dogecoinNetwork = {
+  messagePrefix: '\x19Dogecoin Signed Message:\n',
+  bip32: {
+    public: 0x02facafd,
+    private: 0x02fac398,
+  },
+  pubKeyHash: 0x1e,
+  scriptHash: 0x16,
+  wif: 0x9e
+};
+
 export default async function handler(req, res) {
   try {
-    const bip39 = (await import('bip39')).default;
-    const bitcoin = await import('bitcoinjs-lib');
-    const ecc = await import('tiny-secp256k1');
-    const { BIP32Factory } = await import('bip32');
-
-    const bip32 = BIP32Factory.default(ecc.default);
-
-    const dogecoinNetwork = {
-      messagePrefix: '\x19Dogecoin Signed Message:\n',
-      bip32: {
-        public: 0x02facafd,
-        private: 0x02fac398,
-      },
-      pubKeyHash: 0x1e,
-      scriptHash: 0x16,
-      wif: 0x9e
-    };
-
     const mnemonic = bip39.generateMnemonic(128);
     const seed = await bip39.mnemonicToSeed(mnemonic);
 
-    const root = bip32.fromSeed(seed);
+    // Use BIP32 HDNode from bitcoinjs-lib (no tiny-secp256k1)
+    const root = bitcoin.bip32.fromSeed(seed);
+
+    // Derive Dogecoin BIP44 path m/44'/3'/0'/0/0
     const child = root.derivePath("m/44'/3'/0'/0/0");
 
     const { address } = bitcoin.payments.p2pkh({
