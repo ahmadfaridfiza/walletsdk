be4d1856c14621f00fc5c030f08f2a2fcfe3a6ad
ahmadfaridfiza <ahmadfaridfiza@gmail.com>
2025-07-24 20:18:24 +0800

    ap

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,19 +1,20 @@
-import { loadAccountFromMnemonic } from "@aptos-labs/ts-sdk";
-import * as bip39 from 'bip39';
+import { Account } from "@aptos-labs/ts-sdk";
+import * as bip39 from "bip39";
 
 export default async function handler(req, res) {
   try {
     const mnemonic = bip39.generateMnemonic(128);
-
     const derivationPath = "m/44'/637'/0'/0'/0'";
 
-    // Load Account from Mnemonic using loadAccountFromMnemonic (v1.0+)
-    const account = await loadAccountFromMnemonic({
+    const account = Account.fromDerivationPath({
       mnemonic,
       path: derivationPath,
     });
 
-    const privateKeyHex = Buffer.from(account.signingKey.secretKey).toString('hex').slice(0, 64);
+    const privateKeyHex = Buffer
+      .from(account.signingKey.secretKey)
+      .toString("hex")
+      .slice(0, 64);
 
     res.status(200).json({
       mnemonic,
